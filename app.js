976b62eb8b5759a6a2a4c594e95fc04999b89272
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-21 01:19:34 +0600

    modified

--- app.js
+++ app.js
@@ -2,47 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.use(express.urlencoded()); //body-parser
 app.set('view engine', 'ejs'); //ejs
+
+
 
 app.get('/', (req, res) => {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-
-        default:
-            console.log("Error!");
-          
-    }
 
 
-    res.render('list', { kindOfDay: day });
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
+    var day = today.toLocaleDateString("en-US", options);
+    res.render('list', { kindOfDay: day, newListItems: items });
+
+});
+
+
+app.post("/", function(req,res){
+    var item = req.body.newItem
+    
+    items.push(item);
+    res.redirect("/");
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
