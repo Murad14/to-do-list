2dcd446e8d4498b171755a6afe414b71646bed80
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-28 02:27:14 +0600

    modified

--- app.js
+++ app.js
@@ -80,7 +80,18 @@ app.post("/", function (req, res) {
 
     res.redirect('/');
 
-})
+});
+
+app.post("/delete", function (req, res){
+    const checkedItemId = req.body.checkbox;
+
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(!err){
+            console.log("Succesfully Deleted");
+            res.redirect('/');
+        }
+    })
+});
 
 app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Work List", newListItems: workItems });
