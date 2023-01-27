05f8a8977179416a6a00394a2cbc7f7df01208d7
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-27 21:13:49 +0600

    modified

--- app.js
+++ app.js
@@ -70,15 +70,15 @@ app.get('/', (req, res) => {
 
 app.post("/", function (req, res) {
 
-    let item = req.body.newItem
+    const itemName = req.body.newItem;
 
-    if (req.body.list === "Work List") {
-        workItems.push(item)
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
+    const item = new Item ({
+        name : itemName
+    });
+
+    item.save();
+
+    res.redirect('/');
 
 })
 
