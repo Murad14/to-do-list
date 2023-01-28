12e2db4e8014d749f6287df1c6e6e76a30a5311d
Murad <49407545+Murad14@users.noreply.github.com>
2023-01-28 20:15:46 +0600

    modified

--- app.js
+++ app.js
@@ -100,14 +100,22 @@ app.get("/:customListName", function(req, res){
 app.post("/", function (req, res) {
 
     const itemName = req.body.newItem;
+    const listName = req.body.list;
 
     const item = new Item({
         name: itemName
     });
 
-    item.save();
-
-    res.redirect('/');
+    if (listName === "Today"){
+        item.save();
+        res.redirect('/');
+    }else{
+        List.findOne({name: listName}, function(err, foundList){
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
 
 });
 
